4f0a2347722422910a5b56e7cd8707c299848e7d
Heloisa Zen <heloisa.crepaldi@hotmail.com>
2024-09-20 16:48:05 -0300

    Desafio concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,19 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+
+            // Implementado 20/09/2024
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
+
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+
+                //Implementado 20/09/2024
+                throw new Exception("A quantidade de hóspedes não deve ultrapassar a capacidade da suíte");
             }
         }
 
@@ -36,22 +40,25 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+
+            //Implementado 20/09/2024
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+
+            //implementado 20/09/2024
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+
+            //implementado 20/09/2024
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor * 0.90M;
             }
 
             return valor;
